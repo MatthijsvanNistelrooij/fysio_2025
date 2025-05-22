d48c7dbe64121efa17e1a3f209e3bf085731a928
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-05-22 23:41:04 +0200

    added login

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image"
-import logo from "@/public/logo.png"
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">Welcome back!</div>
